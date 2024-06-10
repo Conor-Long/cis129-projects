ae28af7fa228dc8fab66ffac47beaa1d6b240bdc
Conor-Long <clong15@mail.pima.edu>
2024-06-09 22:33:09 -0700

    Remove print "Hello, World!", added comments, added printed story
    Remove print "Hello, World!"
    Added an author tag 
    Added a program description
    Added a summary of changes
    Added a 4 line printed short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Conor Long
+# My first Python lab in CIS129
+# Prints a simple short story
+
+print('Emily emerged from the ruins, her boots echoing through the wasteland.') 
+print('The remnants of a civilization lay scattered around her, the once-vibrant structures now husks.')
+print('As she ventured into the land, the silence was broken only by the wind whispering secrets through the crumbling walls.') 
+print('A crow circled overhead, its caw was a harbinger of the solitude that cursed the land.')
