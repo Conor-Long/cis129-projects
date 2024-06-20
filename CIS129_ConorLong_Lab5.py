ffe4cdf692025d808293715cdd63cf9243df8a79
Conor-Long <clong15@mail.pima.edu>
2024-06-19 17:31:49 -0700

    Update CIS129_ConorLong_Lab5.py
    final changes

--- CIS129_ConorLong_Lab5.py
+++ CIS129_ConorLong_Lab5.py
@@ -10,27 +10,37 @@ counter = 1
 todayBottles = 0
 totalPayout = 0
 keepGoing = 'y'
+
+# Step 1: Declare variables below 
+NBR_OF_DAYS = 7
+totalBottles = 0
+counter = 1
+todayBottles = 0
+totalPayout = 0
+keepGoing = 'y'
 	
 # Step 2: Loop to run program again
 while counter <= NBR_OF_DAYS:
     for todayBottles in range(7):
-        print('Enter number of bottles returned Day ' + str(counter) + ':')
-        todayBottles = input()
-        print('Day ' + str(counter) + ' payout is: $' + str(int(todayBottles) * .10))
-        print('Day ' + str(counter) + ' bottles added to the total count is : ' + str(todayBottles))
+        todayBottles = input('Enter number of bottles for day #' + str(counter) + ': ')
         totalBottles += int(todayBottles)
         counter += 1
+
     # Step 3: Code to set value of variables 
     totalPayout = 0
     PAYOUT_PER_BOTTLE = .10
     totalPayout = totalBottles * PAYOUT_PER_BOTTLE
-    print('The total number of bottles is: ' + str(totalBottles) + f'\nTotal Payout: ${totalPayout:.2f}')
-    print('Do you want to enter another week\'s worth of data?')
-    print('(Enter y or n)')
-    keepGoing = input()
+    print('\nThe total number of bottles collected is ' + str(totalBottles) + f'\nThe total paid out is: ${totalPayout:.2f}')
+    print('\nDo you want to enter another week\'s worth of data?')
+    keepGoung = input('(Enter y or n): ')
+    print()
+
+    # Restarts the loop if keepGoing equals 'y'
     if keepGoing == 'y':
         counter = 1
         continue
     else:
         break	
+
+
 
